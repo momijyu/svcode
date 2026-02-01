768efb1f0396b717d10b795af0442cb689fde648
momijyu <kaede.t0204@gmail.com>
2026-02-01 09:25:24 +0900

    jisshu2要らなかった

--- jisshu2/cnt_odd.py
+++
@@ -1,9 +0,0 @@
-numbers= [2, 3, 1, 4, 4, 0, 2, 5, 4, 3]
-cntodd= 0
-cnteven= 0
-for i in range(len(numbers)):
-    if numbers[i]%2 == 1:
-        cntodd += 1
-    else:
-        cnteven += 1
-print("odd:",cntodd, "\neven:",cnteven)
--- jisshu2/math_sum_average.py
+++
@@ -1,6 +0,0 @@
-values= [23.5, 35.2, 73.5, 47.6]
-sum_num= 0
-for i in range(len(values)):
-    sum_num += values[i]
-ave= sum_num /len(values)
-print("合計値:",sum_num,"\n平均値:",ave)
