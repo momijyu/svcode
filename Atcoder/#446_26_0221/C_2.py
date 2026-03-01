67a42c1c817c9bdd96d83d11dbd9824e7374e40d
momijyu <kaede.t0204@gmail.com>
2026-03-01 13:18:42 +0900

    定期更新

--- Atcoder/#446_26_0221/C_2.py
+++ Atcoder/#446_26_0221/C_2.py
@@ -11,7 +11,7 @@ for i in range(t):
             idx = z
             while a[idx] < 0:
                 a[idx+1] += a[idx]
-                idx +=1
+                idx += 1
         if j > d-1:
             a[z]= 0
             z+= 1
